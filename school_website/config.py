780215659efa2c6d7997a3383996d3c73e0230eb
shibukgplr <114048520+shibukgplr@users.noreply.github.com>
2025-08-20 17:43:51 +0530

    Delete school_website directory

--- school_website/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-hard-to-guess-string'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://username:password@localhost/school_website'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/uploads')
